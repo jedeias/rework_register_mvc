5bca6489c6d96e02e7417b09a756b785c252c55f
jedeias_Luiz <jedeiasluiz3@gmail.com>
2023-04-07 20:19:24 -0300

    Make a regeister page

---
+++ app/controller/insert.php
@@ -0,0 +1,12 @@
+<?php
+
+$name = $_POST["name"];
+$surName = $_POST["surName"];
+$sex = $_POST["sex"];
+$password = $_POST["password"];
+$email = $_POST["email"];
+$cpf = $_POST["cpf"];
+
+var_dump($_POST);
+
+?>
